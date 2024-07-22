25a7062f81b71744f364afb1b6b3f01f5f670ae3
srkimdev <emirated3858@gmail.com>
2024-07-22 14:15:40 +0900

    Initial Commit

---
+++ Avez Vous/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Avez Vous
+//
+//  Created by 김성률 on 7/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
